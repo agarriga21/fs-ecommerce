0ddb6a505a5b218c71f249b5629f98a77cea90a6
agarriga21 <78110107+agarriga21@users.noreply.github.com>
2022-05-10 15:18:38 -0500

    Update my-app-config.ts

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -1,11 +1,11 @@
 export default {
 
     oidc: {
-        clientId: '0oa4c8dn9p65fmC7O5d7',
-        issuer: 'https://dev-5165573.okta.com/oauth2/default',
-        redirectUri: 'https://ecommerce-front.azurewebsites.net/login/callback',
+        clientId: '0oa4zmkhlfczZ6hNu5d7',
+        issuer: 'https://dev-4042123.okta.com/oauth2/default',
+        redirectUri: 'https://alexcomfrontend.azurewebsites.net/login/callback',
         //redirectUri: 'http://localhost:4200/login/callback',
-        scopes: ['openid', 'profile', 'email', 'groups']
+        scopes: ['openid', 'profile', 'email']
     }
 
 }
